4b3f9bbac9c13f9502d6883207f845f1fd433e21
Roberto Fuentes <155397372+roberfg@users.noreply.github.com>
2026-04-23 00:24:22 -0400

    feat: Add bazzite file

--- 00-TODO.md
+++
@@ -1,4 +0,0 @@
-# TODO - runs
-
-- Agregar soporte para otros desktops Linux utilizados
-- Mejorar manejo de errores en scripts
---
+++ bazzite.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ujust update
+
+brew install stow maven fzf ripgrep openjdk go rustup nvm neovim
+
+flatpak install flathub io.mpv.Mpv
+flatpak install flathub org.qbittorrent.qbittorrent
+flatpak install flathub org.wezfurlong.wezterm
